919fcb19214e96b533a219021f4900789f081ce4
gloryhkg <59158030+gloryhkg@users.noreply.github.com>
2020-02-27 22:40:12 +0800

    try 2015 script

--- hkgweb/compressed.js
+++ hkgweb/compressed.js
@@ -1,1 +1,16 @@
-(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
+
+   function escapeRegExp(string){
+    return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+}
+
+if (window.navigator.standalone) {
+    var local = document.domain;
+    $(document).on('click', 'a', function(event) {
+        var a = $(this).attr('href');
+        if ( a.match('https?://(www\\.)?' + escapeRegExp(local))){
+            event.preventDefault();
+            document.location.href = a;
+        }
+    });
+}
+    
